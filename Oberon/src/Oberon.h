4f369766fadba34a63892732cd2d085da47287c4
svw88 <svw88@hotmail.com>
2021-05-15 22:43:33 -0300

    Premake

--- Oberon/src/Oberon.h
+++ Oberon/src/Oberon.h
@@ -3,6 +3,7 @@
 // For use by Oberon Applications
 
 #include "Oberon/Application.h"
+#include "Oberon/Log.h"
 
 // ------Entry Point-------------------------------
 #include "Oberon/EntryPoint.h"
--- Oberon/src/Oberon/EntryPoint.h
+++ Oberon/src/Oberon/EntryPoint.h
@@ -6,6 +6,11 @@ extern Oberon::Application* Oberon::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Oberon::Log::Init();
+	OB_CORE_WARN("Initialized Log!");
+	int a = 5;
+	OB_INFO("Hello! Var={0}", a);
+
 	auto app = Oberon::CreateApplication();
 	app->Run();
 	delete app;
